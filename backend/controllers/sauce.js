d3f1a5fa3a628188569ddd20a97fd05d7830793f
git504 <git504@mailo.com>
2021-07-09 14:36:47 +0200

    middleware importations

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,5 +1,5 @@
-/******** Importations
+/******** Importations *********/
 const Sauce = require('../models/Sauce');
 const fs = require('fs');
 
-/******** Exportations de méthodes pour les routes
+/******** Exportations de méthodes pour les routes *********/
---
+++ backend/middleware/multer-config.js
@@ -0,0 +1,11 @@
+
+/******** Importations *********/
+const multer = require('multer');
+
+const MIME_TYPES = {
+	'image/jpg': 'jpg',
+	'image/jpeg': 'jpg',
+	'image/png': 'jpg'
+};
+
+/******** Objet de configuration *********/
