deba1efcd6252b2b4b6cee627224297cd48ad198
git504 <git504@mailo.com>
2021-07-12 18:05:58 +0200

    schema USER p4 note de cadrage

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,3 +3,13 @@ const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
 /******** Création du schéma User *********/
+const userSchema = mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+/******** 1 Utilisateur = 1 email *********/
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model("User", userSchema);
