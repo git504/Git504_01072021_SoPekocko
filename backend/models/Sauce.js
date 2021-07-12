4ae3cff2ae5e81a1cb26f4b5fa74f301075d9e92
git504 <git504@mailo.com>
2021-07-12 18:04:04 +0200

    schema SAUCE p3 note de cadrage

--- backend/models/Sauce.js
+++ backend/models/Sauce.js
@@ -2,3 +2,19 @@
 const mongoose = require("mongoose");
 
 /******** Creation d'un schéma Sauce *********/
+const sauceSchema = mongoose.Schema({
+  userId: { type: String, required: true },
+  name: { type: String, required: true },
+  manufacturer: { type: String, required: true },
+  description: { type: String, required: true },
+  mainPepper: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  heat: { type: Number, required: true },
+  likes: { type: Number, default: 0 },
+  dislikes: { type: Number, default: 0 },
+  usersLiked: { type: [String], default: [] },
+  usersDisliked: { type: [String], default: [] },
+});
+
+/******** Creation d'un schéma Sauce *********/
+module.exports = mongoose.model("Sauce", sauceSchema);
