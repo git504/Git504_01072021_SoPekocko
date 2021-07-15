1091bb1e25af8f4403efaee6bf3e3483c236220e
git504 <git504@mailo.com>
2021-07-15 08:46:18 +0200

    minorfix nosave import usercontrolllers

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -4,3 +4,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 /******** Middlewares d'authentification *********/
+/******** Importations *********/
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
